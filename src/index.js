05c7c58bdefa5872192782f1c7168358bb7ce186
fracalo <fracalo@users.noreply.github.com>
2016-12-15 10:48:53 +0100

    added pug and scss to the workflow

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './path-data-polyfill'
-import svgContour from './svg-contour'
-
-if (typeof window === 'undefined')
-  throw new Error('svgContour is developed for browser only, \n' +
-  'if you want to create an offset pathData from an SVG path data outside the DOM\n' +
-  'take look at src/utils/{redraw-steep-curve.js, contour-path-data.js} files')
-
-window.svgContour = svgContour
